9527fd0f8d1b0bb687e77ff3f3584ea2a9a7ddf7
britoalana <alana.brito@al.estudante.senai.br>
2023-09-29 14:05:30 -0300

    Criação das pastas do projeto e conexão com o banco

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const flash = require("express-flash");
+
+const app = express();
+const port = 3333;
+
+const conn = require("./db/conn");
